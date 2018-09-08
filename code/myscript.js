03d60958b31d6f4442a7f285d6968e3943ad2e69
A-Game <javascript@adamheldring.com>
2018-09-08 23:11:22 +0200

    Added real content
    Added unique images, titles and taglines.

---
+++ code/myscript.js
@@ -0,0 +1,6 @@
+// Newspaper assignment script file
+console.log('Hello World');
+
+// Listen for hover on articles, move up a couple of pixels and add drop-shadow
+
+// Listen for click on articles and make hero the clicked article
