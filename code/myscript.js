866f1bb501fbdec935ff9a550857ec54eb4972ac
A-Game <javascript@adamheldring.com>
2018-09-10 10:51:44 +0200

    Added hero opacity transition

--- code/myscript.js
+++ code/myscript.js
@@ -1,21 +1,37 @@
 // Adam's Newspaper assignment script file
 
-// Listen for hover on articles, move up a couple of pixels and add drop-shadow
+// Changes hero image to hovered articles image
+// for the sake of touch screens it does the same for clicked images,
+// also console-logs what images was clicked.
 
-// Listen for click on articles and make hero the clicked article
+// Changes opacity of hero in relation to how far the page is scrolled
 
 window.onload = function () {
 
 var hero = document.getElementById('hero-image');
+var heroHeight = hero.height;
+var scrollPosition;
 
   function changeHero(e) {
     var target = e.target;
     for (var i = 1; i < 10; i++) {
       if (target.dataset.articleNr == i) {
         hero.src = "./images/" + i +".png";
+
+        // Console logs what image was clicked
+        if (e.type == "click") {
+          console.log("You clicked article nr " + i);
+        }
       }
     }
   }
+
+    function transformHero(e) {
+      scrollPosition = document.documentElement.scrollTop;
+      if (scrollPosition < heroHeight) {
+        hero.style.opacity = 1 - (scrollPosition / heroHeight) * 2;
+      }
+    }
 
   document.getElementById('article1').addEventListener('click', changeHero);
   document.getElementById('article2').addEventListener('click', changeHero);
@@ -36,4 +52,6 @@ var hero = document.getElementById('hero-image');
   document.getElementById('article7').addEventListener('mouseover', changeHero);
   document.getElementById('article8').addEventListener('mouseover', changeHero);
   document.getElementById('article9').addEventListener('mouseover', changeHero);
+
+  document.addEventListener('scroll', transformHero)
 }
